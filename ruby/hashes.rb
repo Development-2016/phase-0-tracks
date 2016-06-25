9f2e895697e95e79a78e794e8374929323bdd807
Rog <Rog@Rogers-Mac.local>
2016-06-25 10:30:20 -0500

    initial commit

---
+++ ruby/hashes.rb
@@ -0,0 +1,5 @@
+# pseudocode and write a program that will allow an interior designer 
+# to enter the details of a given client: the client's 
+# name, age, number of children, decor theme, and so on 
+# (you can choose your own as long as it's a good mix of string, integer, and boolean data).
+
