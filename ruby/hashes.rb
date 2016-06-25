784b85315ea8758e619a5f19107a5505415fc7e1
Rog <Rog@Rogers-Mac.local>
2016-06-25 14:18:33 -0500

    added input update if needed

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -10,17 +10,56 @@ client = {
 	decor_theme:'',
 	fav_color:'',
 }
-
 # prompt the user to input all the data
 puts "What is the client's name?"
 client[:name] = gets.chomp
+
 puts "What is the client's age"
-client[:age] = gets.chomp
+age = gets.chomp
+client[:age] = age.to_i
+
 puts "Does the client have children? (true or false)"
-client[:children] = gets.chomp
+children = gets.chomp
+
+if children == "true"
+	client[:children] = true
+else
+client[:children] = false
+end
+
 puts "What is the client's decor theme?"
 client[:decor_theme] = gets.chomp
+
 puts "What is the client's favorite color?"
 client[:fav_color] = gets.chomp
 
-p client
+#p client
+
+# let the client decide if what symbol they would like to change
+# input that information in the client hash
+# if they choose not to change anything, stop when they say 'stop'
+puts "Would you like to update client information?"
+input = gets.chomp
+
+
+
+
+if input == "yes" 
+	puts "What data would you like to update?"
+	update_data = gets.chomp
+	puts "What is the update?"
+	new_update = gets.chomp
+	client[update_data.to_sym] = new_update
+	p client
+elsif 
+ 	 input == "none"
+ 	 p client
+end
+
+
+
+
+
+
+
+
