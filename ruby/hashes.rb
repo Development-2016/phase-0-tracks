2782abf096ba77cc8d736d55b691eb82063c4d88
Rog <Rog@Rogers-Mac.local>
2016-06-25 10:43:06 -0500

    added UI

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -3,3 +3,24 @@
 # name, age, number of children, decor theme, and so on 
 # (you can choose your own as long as it's a good mix of string, integer, and boolean data).
 
+client = {
+	name: '',
+	age:'',
+	children:'',
+	decor_theme:'',
+	fav_color:'',
+}
+
+# prompt the user to input all the data
+puts "What is the client's name?"
+client[:name] = gets.chomp
+puts "What is the client's age"
+client[:age] = gets.chomp
+puts "Does the client have children? (true or false)"
+client[:children] = gets.chomp
+puts "What is the client's decor theme?"
+client[:decor_theme] = gets.chomp
+puts "What is the client's favorite color?"
+client[:fav_color] = gets.chomp
+
+p client
