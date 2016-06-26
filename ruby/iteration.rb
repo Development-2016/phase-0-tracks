1d7bdff1317fb3435f19f7ea780be1eef4d1c76a
Rog <Rog@Rogers-Mac.local>
2016-06-25 19:24:29 -0500

    added blocks

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,2 +1,9 @@
 # write method that takes a block
 # method should print out status message before and after block
+
+def printer
+	puts "This will print out your block"
+	yield ("Hello", "World")
+end
+
+printer{ |word_1, word_2| puts "Is #{word_1} or even #{word_2} this going to print?"}
