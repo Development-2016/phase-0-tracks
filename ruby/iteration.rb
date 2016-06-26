1f75cc11e6e98fd3a0673694c24ac398edbc85ce
Rog <Rog@Rogers-Mac.local>
2016-06-25 19:38:14 -0500

    defined method

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,9 +1,9 @@
 # write method that takes a block
 # method should print out status message before and after block
 
-def printer
-	puts "This will print out your block"
-	yield ("Hello", "World")
+def say_hello
+	puts "Why, hello there!"
+	yield ("Steve","Tania")
 end
 
-printer{ |word_1, word_2| puts "Is #{word_1} or even #{word_2} this going to print?"}
+say_hello { |name1| puts "Great to see you, #{name1} and #{name2}!" }
