bd157ba266baf06d39b07a4a1dd73634b2259b2c
Rog <Rog@Rogers-Mac.local>
2016-06-25 19:44:13 -0500

    changed names and method name

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,9 +1,9 @@
 # write method that takes a block
 # method should print out status message before and after block
 
-def say_hello
-	puts "Why, hello there!"
-	yield("Stever", "Tiahna")
+def printer
+	puts "I hope this prints..."
+	yield("John", "Thomas")
 end
 
-say_hello { |name1, name2| puts "Great to see you, #{name1} and #{name2}!" }
+printer { |name1, name2| puts "Can you read this, #{name1} and #{name2}?" }
