970e63d479e27cbf84c1edfabc97f9caa8ce2f87
Rog <Rog@Rogers-Mac.local>
2016-06-27 13:08:54 -0500

    added next character of name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,8 +1,13 @@
 
 name = "Felicia Torres"
-p name.chars
+# split and reverse names
+reverse_name = name.split(' ').reverse
+join_name = reverse_name.join(' ')
+swapped_name = join_name
+p swapped_name
 
-
+array1 = swapped_name.chars.map!{|x| x.next}.join('')
+p array1
 
 def next_vowel(word)
 	word_2 = word.split('')
