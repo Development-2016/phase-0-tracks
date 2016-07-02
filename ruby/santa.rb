bcb6442cfc517812c5b76da5596f02c44119c8dc
Rog <Rog@Rogers-Mac.local>
2016-07-02 10:43:04 -0500

    added methods: initialize, speak and eat milk and cookies

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,1 +1,21 @@
 # Attributes
+
+class Santa
+
+	def initialize
+		puts "Initializing Santa instance..."
+	end
+
+	def speak
+		puts "Ho, ho, ho! Happy holidays!"
+	end
+
+	def eat_milk_and_cookies(type_of_cookie)
+		puts "That was a good #{type_of_cookie}!"
+	end
+
+end
+
+big_santa = Santa.new
+big_santa.speak
+big_santa.eat_milk_and_cookies("oatmeal")
