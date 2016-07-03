d2d617e92526cfb33b7b94d36063cd92991ea48c
Rog <Rog@Rogers-Mac.local>
2016-07-03 17:27:36 -0500

    added methods and attributes

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,1 +1,25 @@
 # Designing and Implementing a class
+
+# Design a class representing anything you like
+
+class Watch
+
+	def initialize
+		@brand
+		@color
+		@price
+	end
+
+	def tell_time
+		puts "The time is " + rand(12) ":" + rand(60)
+	end
+
+	def shoots_laser
+		puts "*shoots laser beam*"
+	end
+
+	def set_alarm(alarm)
+		puts "Alarm set at #{alarm}"
+	end
+	
+end
