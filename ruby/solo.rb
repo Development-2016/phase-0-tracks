2c4d52d9180b681a3b2256609f197add511af18b
Rog <Rog@Rogers-Mac.local>
2016-07-03 17:36:11 -0500

    added reader and accessor

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -4,14 +4,17 @@
 
 class Watch
 
-	def initialize
-		@brand
-		@color
-		@price
+	attr_reader :brand
+	attr_accessor :color, :price
+
+	def initialize(brand, color, price)
+		@brand = brand
+		@color = color
+		@price = price
 	end
 
 	def tell_time
-		puts "The time is " + rand(12) ":" + rand(60)
+		puts "The time is " + rand(12) + ":" + rand(60)
 	end
 
 	def shoots_laser
@@ -21,5 +24,11 @@ class Watch
 	def set_alarm(alarm)
 		puts "Alarm set at #{alarm}"
 	end
-	
+
 end
+
+boss = Watch.new("Seals", "Black", "1000")
+p boss
+p boss.brand
+p boss.color
+p boss.price
