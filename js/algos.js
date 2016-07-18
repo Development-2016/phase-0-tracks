1c7866225bbdd338f541ae7dd47786272d49c396
Rog <Rog@Rogers-Mac.local>
2016-07-18 09:53:17 -0500

    added long item function

--- js/algos.js
+++ js/algos.js
@@ -1,2 +1,16 @@
 // write a function that takes an array of words for phrases
 // and returns longest word or phrase in the array.
+
+// function takes an array
+// get the character length of each item in array
+// sort the array from least to longest
+// return the last item in array
+var phrases = ["long phrase","longest phrase","longer phrase"];
+
+function longest_item(){
+	phrases.split(',')//.sort.pop; could have used this for shorter code...
+	phrases.sort;
+	phrases.pop;
+}
+
+console.log(longest_item)
